1a0d7560ad02801a6714093ba0a488bc73ce87e2
FilipeLipe <62721945+FilipeLipe@users.noreply.github.com>
2021-04-20 19:24:36 -0300

    ALTERAÇÂO 00002

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,7 +15,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/index', (req, res) => res.render('pages/index'))
-  .get('/inicio', (req, res) => res.render('pages/inicio'))
+  //.get('/inicio', (req, res) => res.render('pages/inicio'))
   .get('/inscreva-se', (req, res) => res.render('pages/inscreva-se'))
   .get('/criaTarefa', (req, res) => res.render('pages/criaTarefa'))
   .get('/criaEvento', (req, res) => res.render('pages/criaEvento'))
@@ -16,4 +23,30 @@ express()
   .get('/eventos', (req, res) => res.render('pages/eventos'))
   .get('/perfil', (req, res) => res.render('pages/perfil'))
   .get('/tarefas', (req, res) => res.render('pages/tarefas'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM tarefas');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/inicio', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM tarefas');
+      const results = { 'results': (result) ? result.rows : null};
+      console.log(results);
+      res.render('pages/inicio', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
